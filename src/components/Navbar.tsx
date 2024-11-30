03d5651c446252f438eba792d4cb232b0a335e5c
Rodney Garraway <ragprice@Rodney--MacBook-Pro.local>
2024-11-30 00:03:56 -0400

    Move navigation links to the right side next to Post Ad button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,19 +26,6 @@ const Navbar: React.FC = () => {
             Vincy Ads
           </Link>
 
-          {/* Navigation Links */}
-          <div className="hidden md:flex items-center space-x-6 mr-8">
-            <Link to="/" className={`${isActive('/')} transition duration-200`}>
-              Home
-            </Link>
-            <Link to="/about" className={`${isActive('/about')} transition duration-200`}>
-              About
-            </Link>
-            <Link to="/contact" className={`${isActive('/contact')} transition duration-200`}>
-              Contact
-            </Link>
-          </div>
-
           {/* Search Bar */}
           <div className="flex-1">
             <form onSubmit={handleSearch} className="flex max-w-xl">
@@ -58,8 +45,19 @@ const Navbar: React.FC = () => {
             </form>
           </div>
 
-          {/* Action Buttons */}
+          {/* Navigation Links and Action Buttons */}
           <div className="flex items-center space-x-4 ml-8">
+            <div className="hidden md:flex items-center space-x-6">
+              <Link to="/" className={`${isActive('/')} transition duration-200`}>
+                Home
+              </Link>
+              <Link to="/about" className={`${isActive('/about')} transition duration-200`}>
+                About
+              </Link>
+              <Link to="/contact" className={`${isActive('/contact')} transition duration-200`}>
+                Contact
+              </Link>
+            </div>
             <Link
               to="/post-ad"
               className="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600 transition duration-200"
